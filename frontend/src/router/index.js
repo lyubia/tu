44e4f12d8a41a37a2ff3664f694592574e57664e
lyubia <1459075532@qq.com>
2026-03-16 21:27:59 +0800

    feat: 开始简化项目 - 备份当前状态

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,7 +7,6 @@ import SolutionDetail from '../views/SolutionDetail.vue'
 import SolutionCompare from '../views/SolutionCompare.vue'
 import RequirementCenter from '../views/RequirementCenter.vue'
 import RecommendationCenter from '../views/RecommendationCenter.vue'
-import ProviderProducts from '../views/ProviderProducts.vue'
 import AdminRoles from '../views/AdminRoles.vue'
 import AdminAudit from '../views/AdminAudit.vue'
 import AdminDashboard from '../views/AdminDashboard.vue'
@@ -33,7 +32,6 @@ const routes = [
   { path: '/admin/roles', name: 'AdminRoles', component: AdminRoles },
   { path: '/admin/audit', name: 'AdminAudit', component: AdminAudit },
   { path: '/admin/dashboard', name: 'AdminDashboard', component: AdminDashboard },
-  { path: '/provider/products', name: 'ProviderProducts', component: ProviderProducts },
   { path: '/partner', name: 'PartnerConsole', component: PartnerConsole }
 ]
 
